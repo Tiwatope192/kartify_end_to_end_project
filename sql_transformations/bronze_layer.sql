bc85b8e8db89031f7e4dc667a1d0a08f99285c42
peju sokunle <pejutope92@gmail.com>
2026-04-13 21:42:11 +0200

    bronze_layer

---
+++ sql_transformations/bronze_layer.sql
@@ -0,0 +1,12 @@
+ -- Raw (Bronze) layer: no transformations applied; used only to query and verify data accessibility.
+
+select * from DATABASE3.BRONZE."categories";
+select * from DATABASE3.BRONZE."coupons";
+select * from DATABASE3.BRONZE."customers";
+select * from DATABASE3.BRONZE."inventory";
+select * from DATABASE3.BRONZE. "order_items";
+select * from DATABASE3.BRONZE. "orders";
+select * from DATABASE3.BRONZE. "payments";
+select * from DATABASE3.BRONZE. "products";
+select * from DATABASE3.BRONZE. "reviews";
+select * from DATABASE3.BRONZE. "shipping";

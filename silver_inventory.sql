5073ec1f5cae205c4e05981554912aca0a815095
peju sokunle <pejutope92@gmail.com>
2026-04-17 13:58:43 +0200

    fix: remove duplicate insert in silver_shipping

--- silver_inventory.sql
+++ silver_inventory.sql
@@ -1,37 +1,25 @@
 
-  CREATE TABLE IF NOT EXISTS DATABASE3.SILVER.INVENTORY AS
-    SELECT 
-  "id",
-    "product_id",
-    "quantity",
-    "reorder_lvl",
-    ("quantity" - "reorder_lvl")                AS Stock_surplus,       -- how much stock is above reorder level
+CREATE OR REPLACE TABLE DATABASE3.SILVER.inventory AS
+SELECT
+    "id" AS id,
+    "product_id" AS product_id,
+    "quantity" AS quantity,
+    "reorder_lvl" AS reorder_lvl,
+
+    ("quantity" - "reorder_lvl") AS stock_surplus,
+
     CASE
-        WHEN "quantity" <= "reorder_lvl"         THEN 'Reorder Now'     -- at or below reorder level
-        WHEN "quantity" <= "reorder_lvl" * 1.5   THEN 'Running Low'     -- within 50% of reorder level
-        ELSE 'In Stock'                                                  -- sufficient stock
-    END                                         AS Stock_status,
+        WHEN "quantity" <= "reorder_lvl" THEN 'Reorder Now'
+        WHEN "quantity" <= "reorder_lvl" * 1.5 THEN 'Running Low'
+        ELSE 'In Stock'
+    END AS stock_status,
+
     TO_TIMESTAMP("created_at" / 1000000000) AS created_date,
     TO_TIMESTAMP("updated_at" / 1000000000) AS updated_date
-    FROM DATABASE3.BRONZE."inventory";
+
+FROM DATABASE3.BRONZE."inventory";
 
 
-
-INSERT INTO  DATABASE3.SILVER.INVENTORY
- SELECT 
-  "id",
-    "product_id",
-    "quantity",
-    "reorder_lvl",
-    ("quantity" - "reorder_lvl")                AS Stock_surplus,       -- how much stock is above reorder level
-    CASE
-        WHEN "quantity" <= "reorder_lvl"         THEN 'Reorder Now'     -- at or below reorder level
-        WHEN "quantity" <= "reorder_lvl" * 1.5   THEN 'Running Low'     -- within 50% of reorder level
-        ELSE 'In Stock'                                                  -- sufficient stock
-    END                                         AS Stock_status,
-    TO_TIMESTAMP("created_at" / 1000000000) AS created_date,
-    TO_TIMESTAMP("updated_at" / 1000000000) AS updated_date
-    FROM DATABASE3.BRONZE."inventory";
 
     SELECT * FROM DATABASE3.SILVER.INVENTORY;
 
